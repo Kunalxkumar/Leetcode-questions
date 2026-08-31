c4cc4b6b689535a297f7aee9b67249fe91950efe
kunal <106957750+Kunalxkumar@users.noreply.github.com>
2026-08-31 20:32:54 +0530

    Time: 400 ms (59.45%) | Memory: 27.6 MB (46.96%) - LeetSync

--- 1685-stone-game-v/stone-game-v.cpp
+++ 1685-stone-game-v/stone-game-v.cpp
@@ -6,7 +6,9 @@ public:
 
     int solve(int l, int r) {
         if (l == r) return 0;
-        if (dp[l][r] != -1) return dp[l][r];
+
+        if (dp[l][r] != -1)
+            return dp[l][r];
 
         int ans = 0;
 
@@ -14,12 +16,21 @@ public:
             int left = pre[k + 1] - pre[l];
             int right = pre[r + 1] - pre[k + 1];
 
-            if (left < right)
+            if (left < right) {
+                // Bob throws away right
+                // Alice keeps left
                 ans = max(ans, left + solve(l, k));
-            else if (left > right)
+            }
+            else if (left > right) {
+                // Bob throws away left
+                // Alice keeps right
                 ans = max(ans, right + solve(k + 1, r));
-            else
-                ans = max(ans, left + max(solve(l, k), solve(k + 1, r)));
+            }
+            else {
+                // Equal: Alice can choose either side
+                ans = max(ans, left + solve(l, k));
+                ans = max(ans, right + solve(k + 1, r));
+            }
         }
 
         return dp[l][r] = ans;
@@ -27,11 +38,14 @@ public:
 
     int stoneGameV(vector<int>& stoneValue) {
         n = stoneValue.size();
-        pre.assign(n + 1, 0);
+
+        // Prefix sums
+        pre.resize(n + 1, 0);
 
         for (int i = 0; i < n; i++)
             pre[i + 1] = pre[i] + stoneValue[i];
 
+        // dp[l][r] = maximum score from stones l...r
         dp.assign(n, vector<int>(n, -1));
 
         return solve(0, n - 1);
